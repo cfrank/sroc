c8094a681a8403b9bd0541385ce17f4f6fcb9da6
Chris Frank <chris@cfrank.org>
2019-02-23 00:42:49 -0500

    Re-write api to clean it up
    Signed-off-by: Chris Frank <chris@cfrank.org>

--- include/sroc.h
+++ include/sroc.h
@@ -9,95 +9,84 @@
 #include <stdio.h>
 
 enum sroc_error {
-        ERRNOMEM = -1,
-        ERRNOTFOUND = -2,
-        ERRIO = -3,
+        ERRNOKEY = -1,
+        ERRNOSECTION = -2,
+        ERRNOMEM = -3,
+        ERRIO = -4,
 };
 
-enum sroc_data_type {
+enum sroc_type {
         ARRAY,
         BOOL,
         NUMBER,
-        OBJECT,
         STRING,
 };
 
-// Forward declare table for use in sroc_type
-struct sroc_table;
+// Forward declare sroc_type for use with parent types
+struct sroc_value;
 
 /**
- * The sroc type contains one of the valid sroc data types.
+ * A sroc array is an array of valid sroc value
  *
- * The object type is an abstraction on top of sroc_table since they perform
- * the same function.
+ * Each item in the array must be equal in type to the rest of the items
  */
-struct sroc_type {
+struct sroc_array {
+        size_t length;
         enum sroc_data_type type;
+        struct sroc_value *items;
+};
+
+/**
+ * The sroc value contains one of the valid sroc values
+ */
+struct sroc_value {
+        enum sroc_type type;
         union {
-                struct sroc_type **array;
+                struct sroc_array *array;
                 bool boolean;
                 int64_t number;
-                struct sroc_table *object;
-                char **string;
+                char *string;
         };
 };
 
 /**
  * A sroc item is a key value type where the key is a single word string and
- * the value is any valid sroc data type
+ * the value is any valid sroc value
  */
 struct sroc_item {
         const char *key;
-        struct sroc_type *value;
+        struct sroc_value *value;
 };
 
 /**
- * A sroc array is an array of valid sroc data types
- *
- * Each item in the array must be equal in type to the rest of the items
- */
-struct sroc_array {
-        size_t count;
-        enum sroc_data_type type;
-        struct sroc_type *items;
-};
-
-/**
- * A sroc table (also refered to as a section or object) is a keyed list of
+ * The sroc table is the basis for both root files and sections. It contains a
+ * key (which can be NULL when no sections exist in the root) and a list of
  * sroc items
+ *
+ * The size is the number of items in that particular section
  */
 struct sroc_table {
+        // If NULL then this is the root
         const char *key;
-        size_t count;
+        size_t size;
         struct sroc_item *items;
 };
 
-/**
- * This is the root of the file. It contains a list of items and tables
- *
- * When NULL is sent as the section argument to any of the sroc_read_*
- * functions the list of items in this struct are queried
- */
-struct sroc_root {
-        struct sroc_item *items;
-        size_t table_count;
-        struct sroc_table *tables;
-};
-
-struct sroc_root *sroc_init(FILE *file);
-struct sroc_root *sroc_create_root(void);
+struct sroc_table *sroc_init(FILE *file);
 struct sroc_table *sroc_create_table(const char *key);
 
-int sroc_read_array(const struct sroc_root *root, const char *section,
+// Get a single section from the root table
+int sroc_get_section(const struct sroc_table *root, const char *section,
+                     struct sroc_table **dest);
+int sroc_read_array(const struct sroc_table *root, const char *section,
                     const char *key, struct sroc_array **dest, size_t *length);
-int sroc_read_bool(const struct sroc_root *root, const char *section,
+int sroc_read_bool(const struct sroc_table *root, const char *section,
                    const char *key, bool *dest);
-int sroc_read_number(const struct sroc_root *root, const char *section,
+int sroc_read_number(const struct sroc_table *root, const char *section,
                      const char *key, int64_t *dest);
-int sroc_read_object(const struct sroc_root *root, const char *section,
-                     const char *key, struct sroc_table **dest);
-int sroc_read_string(const struct sroc_root *root, const char *section,
-                     const char *key, char *dest);
+int sroc_read_string(const struct sroc_table *root, const char *section,
+                     const char *key, char **dest);
 
-void sroc_destroy_root(struct sroc_root *root);
+void sroc_destroy_array(struct sroc_array *array);
+void sroc_destroy_type(struct sroc_type *type);
 void sroc_destroy_table(struct sroc_table *table);
